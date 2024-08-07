e246402cd411dab799ccc66111dc1ac582568a65
Gülay YILMAZ <gulay.yilmaz@developia.io>
2024-08-07 16:37:00 +0300

    refactor: table and pagination responsive

--- components/shared/common/Table.tsx
+++ components/shared/common/Table.tsx
@@ -1,10 +1,12 @@
+"use client";
+
 import React, { useState } from "react";
 import Pagination from "./Pagination";
 
 type Column<T> = {
   id: string;
   label: string;
-  sortable?: boolean; // Ekledik: sıralama özelliğini etkinleştirmek için
+  sortable?: boolean;
 };
 
 type DataItem = {
@@ -18,6 +20,7 @@ type TableProps<T> = {
   rowsPerPage: number;
   currentPage: number;
   onChangePage: (page: number) => void;
+  fixedFirstColumn?: boolean;
 };
 
 const Table = <T extends DataItem>({
@@ -27,6 +30,7 @@ const Table = <T extends DataItem>({
   rowsPerPage,
   currentPage,
   onChangePage,
+  fixedFirstColumn = false,
 }: TableProps<T>) => {
   const [searchText, setSearchText] = useState<string>("");
 
@@ -35,10 +39,9 @@ const Table = <T extends DataItem>({
     direction: string | null;
   }>({
     key: "",
-    direction: "ascending", // İsteğe bağlı olarak başlangıç sıralama yönü
+    direction: "ascending",
   });
 
-  // Sıralama işlevi
   const sortedData = [...data].sort((a, b) => {
     if (a[sortConfig.key] < b[sortConfig.key]) {
       return sortConfig.direction === "ascending" ? -1 : 1;
@@ -49,7 +52,6 @@ const Table = <T extends DataItem>({
     return 0;
   });
 
-  // Sıralama yöntemini değiştirme işlevi
   const requestSort = (key: string) => {
     let direction = "ascending";
     if (sortConfig.key === key && sortConfig.direction === "ascending") {
@@ -59,51 +61,66 @@ const Table = <T extends DataItem>({
   };
 
   return (
-    <div>
-      <table className="min-w-full divide-y divide-gray-200">
-        <thead className="bg-white divide-y divide-gray-200 ">
-          <tr>
-            {columns.map((column, index) => (
-              <th
-                key={index}
-                className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-              >
-                {column.sortable ? (
-                  <button
-                    onClick={() => column.sortable && requestSort(column.id)}
-                    className="flex items-center space-x-1"
-                  >
-                    <span>{column.label}</span>
-                    {sortConfig.key === column.id && (
-                      <span>
-                        {sortConfig.direction === "ascending" ? "▲" : "▼"}
-                      </span>
-                    )}
-                  </button>
-                ) : (
-                  <span>{column.label}</span>
-                )}
-              </th>
-            ))}{" "}
-          </tr>
-        </thead>
-        <tbody className="bg-white divide-y divide-gray-200 text-gray-800">
-          {sortedData.map((item, index) => (
-            <tr key={index}>
+    <div className="">
+      <div className="overflow-x-auto">
+        <table className="min-w-full divide-y divide-gray-200">
+          <thead className="bg-white divide-y divide-gray-200">
+            <tr>
               {columns.map((column, index) => (
-                <td key={index} className="px-6 py-4 whitespace-nowrap">
-                  {item[column.id]}
-                </td>
+                <th
+                  key={index}
+                  className={`px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider ${
+                    fixedFirstColumn && index === 0
+                      ? "sticky left-0 bg-white"
+                      : ""
+                  }`}
+                >
+                  {column.sortable ? (
+                    <button
+                      onClick={() => column.sortable && requestSort(column.id)}
+                      className="flex items-center space-x-1"
+                    >
+                      <span>{column.label}</span>
+                      {sortConfig.key === column.id && (
+                        <span>
+                          {sortConfig.direction === "ascending" ? "▲" : "▼"}
+                        </span>
+                      )}
+                    </button>
+                  ) : (
+                    <span>{column.label}</span>
+                  )}
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <Pagination
-        totalPagesCount={totalPages}
-        currentPage={currentPage}
-        onChangePage={onChangePage}
-      />
+          </thead>
+          <tbody className="bg-white divide-y divide-gray-200 text-gray-800">
+            {sortedData.map((item, rowIndex) => (
+              <tr key={rowIndex}>
+                {columns.map((column, colIndex) => (
+                  <td
+                    key={colIndex}
+                    className={`px-6 py-4 whitespace-nowrap ${
+                      fixedFirstColumn && colIndex === 0
+                        ? "sticky left-0 bg-white"
+                        : ""
+                    }`}
+                  >
+                    {item[column.id]}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className=" flex justify-center py-4">
+        <Pagination
+          totalPagesCount={totalPages}
+          currentPage={currentPage}
+          onChangePage={onChangePage}
+        />
+      </div>
     </div>
   );
 };
--- hooks/usePagination.ts
+++ hooks/usePagination.ts
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useEffect, useState } from "react";
 import { PaginationComponentProps } from "@/components/shared/common/Pagination";
 
 export const DOTS = "...";
@@ -8,46 +8,69 @@ const range = (start: number, end: number) => {
   return Array.from({ length }, (_, idx) => idx + start);
 };
 
+const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: 1280,
+    height: 800,
+  });
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+
+      window.addEventListener("resize", handleResize);
+      handleResize();
+
+      return () => window.removeEventListener("resize", handleResize);
+    }
+  }, []);
+
+  return windowSize;
+};
+
 export const usePagination = ({
   totalPagesCount,
   currentPage,
 }: PaginationComponentProps) => {
-  const siblingCount = 1;
+  const windowSize = useWindowSize();
+  const [siblingCount, setSiblingCount] = useState(1);
+
+  useEffect(() => {
+    if (windowSize.width) {
+      if (windowSize.width < 640) {
+        setSiblingCount(0); // Mobile devices
+      } else if (windowSize.width < 1024) {
+        setSiblingCount(1); // Tablets
+      } else {
+        setSiblingCount(2); // Desktops
+      }
+    }
+  }, [windowSize]);
 
   const paginationRange = useMemo(() => {
-    // Pages count is determined as siblingCount + firstPage + lastPage + currentPage + 2*DOTS
     const totalPageNumbers = siblingCount + 5;
 
-    /*
-      Case 1:
-      If the number of pages is less than the page numbers we want to show in our
-      paginationComponent, we return the range [1..totalPagesCount]
-    */
     if (totalPageNumbers >= totalPagesCount) {
       return range(1, totalPagesCount);
     }
 
-    /*
-    	Calculate left and right sibling index and make sure they are within range 1 and totalPagesCount
-    */
     const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
     const rightSiblingIndex = Math.min(
       currentPage + siblingCount,
       totalPagesCount
     );
 
-    /*
-      We do not show dots just when there is just one page number to be inserted between the extremes of sibling and the page limits i.e 1 and totalPagesCount. Hence we are using leftSiblingIndex > 2 and rightSiblingIndex < totalPagesCount - 2
-    */
     const shouldShowLeftDots = leftSiblingIndex > 2;
     const shouldShowRightDots = rightSiblingIndex < totalPagesCount - 2;
 
     const firstPageIndex = 1;
     const lastPageIndex = totalPagesCount;
 
-    /*
-    	Case 2: No left dots to show, but rights dots to be shown
-    */
     if (!shouldShowLeftDots && shouldShowRightDots) {
       const leftItemCount = 3 + 2 * siblingCount;
       const leftRange = range(1, leftItemCount);
@@ -55,9 +78,6 @@ export const usePagination = ({
       return [...leftRange, DOTS, totalPagesCount];
     }
 
-    /*
-    	Case 3: No right dots to show, but left dots to be shown
-    */
     if (shouldShowLeftDots && !shouldShowRightDots) {
       const rightItemCount = 3 + 2 * siblingCount;
       const rightRange = range(
@@ -67,9 +87,6 @@ export const usePagination = ({
       return [firstPageIndex, DOTS, ...rightRange];
     }
 
-    /*
-    	Case 4: Both left and right dots to be shown
-    */
     if (shouldShowLeftDots && shouldShowRightDots) {
       const middleRange = range(leftSiblingIndex, rightSiblingIndex);
       return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
